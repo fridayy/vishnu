a532d86d99e07560a4e63ca2aa5dc9285dbc8783
fridayy <bnjm@harmless.ninja>
2017-07-03 18:11:35 +0200

    cleanup; project structure modified; projects added

---
+++ vishnu-flight-service/src/main/java/ninja/harmless/vishnu/VishnuFlightServiceApplication.java
@@ -0,0 +1,12 @@
+package ninja.harmless.vishnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VishnuFlightServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VishnuFlightServiceApplication.class, args);
+	}
+}
---
+++ vishnu-passenger-service/src/main/java/ninja/harmless/vishnu/VishnuPassengerServiceApplication.java
@@ -0,0 +1,12 @@
+package ninja.harmless.vishnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VishnuPassengerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VishnuPassengerServiceApplication.class, args);
+	}
+}
