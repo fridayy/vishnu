273f6ece0cb941b203dc094c7b9cba9111e24304
fridayy <bnjm@harmless.ninja>
2017-07-08 18:24:49 +0200

    multiple service and infrastructure modules added

--- vishnu-zipkin/src/test/java/ninja/harmless/vishnu/VishnuZipkinApplicationTests.java
+++ vishnu-zipkin/src/test/java/ninja/harmless/vishnu/VishnuZipkinApplicationTests.java
@@ -7,9 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class VishnuFlightServiceApplicationTests {
+public class VishnuZipkinApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
+
 }
---
+++ vishnu-zuul/src/main/java/ninja/harmless/vishnu/VishnuZuulApplication.java
@@ -0,0 +1,12 @@
+package ninja.harmless.vishnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VishnuZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VishnuZuulApplication.class, args);
+	}
+}
